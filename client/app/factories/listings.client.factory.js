39bf35a13038c956ee75ede2726f02348dd28f3d
Zach <zmurta@ufl.edu>
2017-09-20 12:42:46 -0400

    removed localhost:8080

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get('/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $http.post('/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('/api/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('/api/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('/api/listings/' + id);
       }
     };
 
